972e6925bd117551bb581987d9c43cae7e3160a8
Sergio Daniel Barroso <dannyfixmycomputer@aol.com>
2022-04-19 13:40:54 -0300

    db ready

--- api/src/Controllers/Controllers.js
+++ api/src/Controllers/Controllers.js
@@ -5,6 +5,39 @@ const { Dog, Temperament } = require("../db");
 // GET API
 
 const getApiData = async () => {
-    const infoApi = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key`);
+  const infoApi = await axios.get(
+    `https://api.thedogapi.com/v1/breeds?api_key`
+  );
+  const showApi = infoApi.data.map((parameters) => {
+    return {
+      id: parameters.id,
+      name: parameters.name,
+      weight: parameters.weight.metric.split("-"),
+      Image: parameters.Image.url,
+    };
+    return showApi;
+  });
+
+  const getDataBase = async () => {
+    return await Dog.findAll({
+      include: {
+        model: Temperament,
+        atributtes: ["name"],
+        through: { atributtes: [] },
+      },
+    });
+  };
 };
 
+const join = async () => {
+  const fullApi = getFullApi();
+  const DataBase = getDataBase();
+  const hibrido = fullApi.concat(DataBase);
+  return hibrido;
+};
+
+module.exports = {
+  getFullApi,
+  getDataBase,
+  join,
+};
